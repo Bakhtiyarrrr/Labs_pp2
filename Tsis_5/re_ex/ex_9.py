e813abf017ac67e41995f08ece2ad6152d368027
Bakhtiyarrrr <155830512+Bakhtiyarrrr@users.noreply.github.com>
2024-02-24 16:54:18 +0600

    Update ex_9.py

--- Tsis_5/re_ex/ex_9.py
+++ Tsis_5/re_ex/ex_9.py
@@ -3,4 +3,4 @@ text = input()
 match = re.findall(r"\W*[A-Z][a-z]*",text)
 stroka =" ".join(match)
 print(stroka)
-#изначально вводится сплошная строка по типу HelloMyNameIsBakhtiyar, и между ими ставится пробел
+
