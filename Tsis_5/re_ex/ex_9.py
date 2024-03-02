d486d4ff7026d04e672e71f1309fe7bbdffda8c6
Bakhtiyar <bahtiarmusanov114@gmail.com>
2024-03-02 17:19:32 +0500

    fix

--- Tsis_5/re_ex/ex_9.py
+++ Tsis_5/re_ex/ex_9.py
@@ -1,6 +1,16 @@
 import re
 text = input()
-match = re.findall(r"[A-Z][a-z]+",text)
-stroka =" ".join(match)
+match = re.findall(r"\W*[A-Z][a-z0-9]*",text)
+print(match)
+t = match[0]
+my_list = [t,]
+for x in range(1,len(match)):
+    stroka_1 = ''
+    for y in match[x]:
+        if y != " ":
+            stroka_1 += y
+    my_list.append(stroka_1)        
+
+print(my_list)
+stroka =" ".join(my_list)
 print(stroka)
-#изначально вводится сплошная строка по типу HelloMyNameIsBakhtiyar, и между ими ставится пробел
